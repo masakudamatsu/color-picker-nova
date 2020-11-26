1333993a64d64e87acfe444f9f1dfcb4677c1b64
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-11-26 19:42:11 +0900

    Add getContrastRatio helper function
    Convert RGB values into luminance contrast ratio

---
+++ utils/getContrastRatio.js
@@ -0,0 +1,25 @@
+const getContrastRatio = (r, g, b) => {
+  // See https://www.w3.org/TR/WCAG20-TECHS/G17.html
+  return ((relativeLuminance(r, g, b) + 0.05) / 0.05).toFixed(2);
+
+  function relativeLuminance(r, g, b) {
+    return (
+      0.2126 * normalize(r) + 0.7152 * normalize(g) + 0.0722 * normalize(b)
+    );
+  }
+
+  function normalize(eightBitNumber) {
+    const percentizedValue = percentize(eightBitNumber);
+    if (percentizedValue <= 0.03928) {
+      return percentizedValue / 12.92;
+    } else {
+      return Math.pow((percentizedValue + 0.055) / 1.055, 2.4);
+    }
+  }
+
+  function percentize(eightBitNumber) {
+    return eightBitNumber / 255;
+  }
+};
+
+export default getContrastRatio;
