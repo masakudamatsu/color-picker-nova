a5b74f1a9b6e178a0705afcfbfe32be491b9d492
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-11-27 16:16:46 +0900

    Format the test for Color component with Prettier

--- components/Color.test.js
+++ components/Color.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import {axe} from 'jest-axe';
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { axe } from "jest-axe";
 
-import Color from './Color';
-import getContrastRatio from '../utils/getContrastRatio';
+import Color from "./Color";
+import getContrastRatio from "../utils/getContrastRatio";
 
 const mockColor = {
   redValue: 123,
@@ -20,11 +20,11 @@ const mockProps = {
   intensity: mockColor.intensityValue,
 };
 
-test('renders UI correctly', () => {
-  const {container} = render(
+test("renders UI correctly", () => {
+  const { container } = render(
     <svg>
       <Color {...mockProps} />
-    </svg>,
+    </svg>
   );
   expect(container).toMatchInlineSnapshot(`
     <div>
@@ -42,40 +42,40 @@ test('renders UI correctly', () => {
   `);
 });
 
-test('is accessible', async () => {
-  const {container} = render(
+test("is accessible", async () => {
+  const { container } = render(
     <svg>
       <Color {...mockProps} />
-    </svg>,
+    </svg>
   );
   const results = await axe(container);
   expect(results).toHaveNoViolations();
 });
 
-test('fills the color specified by the red, green, and blue props', () => {
+test("fills the color specified by the red, green, and blue props", () => {
   const expectedColor = `rgb(${mockColor.redValue}, ${mockColor.greenValue}, ${mockColor.blueValue})`;
-  const {getByTestId} = render(
+  const { getByTestId } = render(
     <svg>
       <Color {...mockProps} />
-    </svg>,
+    </svg>
   );
-  expect(getByTestId('color')).toHaveStyle({fill: expectedColor});
+  expect(getByTestId("color")).toHaveStyle({ fill: expectedColor });
 });
 
-test('is located at the position specified by the prop values', () => {
+test("is located at the position specified by the prop values", () => {
   const contrastRatio = getContrastRatio(
     mockColor.redValue,
     mockColor.greenValue,
-    mockColor.blueValue,
+    mockColor.blueValue
   );
   const expectedX = mockColor.intensityValue.toString();
   const expectedY = (100 - (contrastRatio - 1) * 5).toFixed();
 
-  const {getByTestId} = render(
+  const { getByTestId } = render(
     <svg>
       <Color {...mockProps} />
-    </svg>,
+    </svg>
   );
-  expect(getByTestId('color')).toHaveAttribute('x', expectedX);
-  expect(getByTestId('color')).toHaveAttribute('y', expectedY);
+  expect(getByTestId("color")).toHaveAttribute("x", expectedX);
+  expect(getByTestId("color")).toHaveAttribute("y", expectedY);
 });
