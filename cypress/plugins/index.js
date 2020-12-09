e649dd420bbb564800414050f77057fc9363944b
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-12-09 14:02:53 +0900

    Set up cypress-image-snapshot
    Prepare for testing canvas elements

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,12 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+
+// Setting up Cypress Image Snapshot
+const {addMatchImageSnapshotPlugin} = require('cypress-image-snapshot/plugin');
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  addMatchImageSnapshotPlugin(on, config);
+};
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,32 @@
+// Cypress Image Snapshot: Start ////////////////////
+import {addMatchImageSnapshotCommand} from 'cypress-image-snapshot/command';
+
+if (Cypress.config('isInteractive')) {
+  // Skip taking snapshots with Cypress GUI. See https://glebbahmutov.com/blog/open-source-visual-testing-of-components/#local-workflow
+  Cypress.Commands.add('matchImageSnapshot', () => {
+    cy.log('Skipping snapshot 👀');
+  });
+} else {
+  addMatchImageSnapshotCommand({
+    // Avoid the full page snapshot
+    capture: 'viewport', // replace this with 'fullPage' for snapshotting the entire page
+
+    // Allow tiny differences in snapshots
+    customDiffConfig: {threshold: 0.1}, // threshold for each pixel
+    failureThreshold: 0.03, // threshold for entire image
+    failureThresholdType: 'percent', // percent of image or number of pixels
+  });
+}
+
+Cypress.Commands.add('setResolution', size => {
+  if (Cypress._.isArray(size)) {
+    cy.viewport(size[0], size[1]);
+  } else {
+    cy.viewport(size);
+  }
+});
+// Cypress Image Snapshot: End /////////////////////////
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
